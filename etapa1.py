22a704ad843e39af49d62e2b12f7c096fc102cde
Pablo <78887322+monardop@users.noreply.github.com>
2022-01-05 19:41:51 -0300

    Termine de esbozar la logica del programa. Armé la funcion que realiza las operaciones, luego emprolijaré y documentaré el programa

--- etapa1.py
+++ etapa1.py
@@ -1,29 +1,31 @@
-from os import system
-from datetime import datetime
+from os import system, write
+from datetime import datetime, date
 
-def current_datatime()
+def current_datatime():
     now = datetime.now()
     hoy = date(now.year, now.month, now.day).ctime()
     fecha = hoy.split()
     message = now.strftime(f"{fecha[0]} {fecha[1]} %d %H:%M:%S %Y")
     return message
 
-def in_out_Register(op, name, s, d, t, total):
+def in_out_Register(op, name, total):
     todayDate = current_datatime()
     if op == 1:
-        with open("ventas.txt", "a") as f:
-            f.write(f"{name}; {todayDate}; {s}; {d}; {t}; ${total}")
-    elif op == 2:
         with open("registros.txt", "a") as f:
             f.write(f"IN {todayDate}; Encargad@: {name}")
     
-    elif op == 3:
+    elif op == 2:
+        end_line = "#"
         with open("registros.txt", "a") as f:
             f.write(f"OUT {todayDate}; Encargad@: {name} ${total}")
+            f,write(end_line*50)
     
-
-
-
+def reg_client(name, order, total):
+    todayDate = current_datatime()
+    s, d, t, p = order
+    with open("ventas.txt", "a") as f:
+                f.write(f"{name}; {todayDate}; {s}; {d}; {t}; {p}; ${total}")
+    
 def welcome():
     print("Bienvenido a Hamburguesas IT")
     name = input("Ingrese su nombre encargad@: ")
@@ -39,13 +41,64 @@ def operation():
         else:
             break
     return op
-    
+
+def order_validation(type):
+    while True:
+        value = int(input(f"Ingrese cantidad de ordenes de {type}: "))
+        if isinstance(value, int) and value>0:
+            break
+        else: 
+            print("Su dato ingresado es incorrecto, por favor ingrese otra vez.")
+    return value
+
+def load_order():
+    total = 0
+    order = []
+    while True:
+        name = input("Ingrese nombre del cliente: ")
+        s = order_validation("combo simple")
+        total += s*5
+        order.append(s)
+        d = order_validation("combo doble")
+        total += d*6
+        order.append(d)
+        t = order_validation("combo triple")
+        total += t*7
+        order.append(t)
+        h = order_validation("helado McFlurby")
+        total += h*2
+        order.append(t)
+        while True:
+            ans = input("Confirma la transaccion? Y/N")
+            if isinstance(ans, str) and (ans.capitalize == "Y" or ans.capitalize == "N"):
+                break
+            else:
+                print("Respuesta incorrecta")
+        if ans.capitalize == "Y":
+            reg_client(name, order, total)
+            print("\n")
+            return total
+        else: 
+            return 0
+
 def main():
+    total = 0
     name = welcome()
-    op = operation()
-    
+    in_out_Register(1, name, 0)
+    while True:
+        op = operation()
+        if op == 1:
+            total += load_order()
+        elif op == 2:
+            in_out_Register(2, name,total)
+            total = 0
+            print("\n")
+            name = welcome()
+            in_out_Register(1,name,0)
+        else: 
+            in_out_Register(2, name,total)
+            break
+    print("Adios, tenga un buen dia!")
     system("pause")
-
-
 
 main()
