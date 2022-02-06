8cab3f867151fc57f2e7cab071d0b40ac21f6f29
Pablo <78887322+monardop@users.noreply.github.com>
2022-02-06 11:56:57 -0300

    Mejora en legibilidad, añadidos esteticos

--- gui.py
+++ gui.py
@@ -1,7 +1,6 @@
 from tkinter import *
-from tkinter import messagebox
 from tkinter.ttk import Style
-from turtle import bgcolor, width
+
 class App(Frame):
     def __init__(self):
         super().__init__()
@@ -10,7 +9,8 @@ class App(Frame):
 
     def initUI(self):
         self.master.title("Delivery")
-        self.master.config(padx=20, pady=20) #agrego un poco de padding
+        self.master.config(padx=20, pady=20) #agrego un poco de padding a la ventana
+
 
         #configure the grid
         self.columnconfigure(0, pad=10)
@@ -26,6 +26,8 @@ class App(Frame):
         self.rowconfigure(6, pad=3)
         self.rowconfigure(7, pad=50)
 
+
+        #crea las leyendas
         titulo = Label(self, text="-------Pedidos-------", pady=10).grid(row=0, column=2)
         encargado = Label(self, text="Nombre del encargado").grid(column=1)
         comboS = Label(self, text="Combo simple").grid(column=1)
@@ -36,18 +38,17 @@ class App(Frame):
 
         #text input
         #Entry es un label que permite ingresar texto en pequeñas cantidades
-        en = Entry(self, width=7).grid(column=3, row=1)
-        cS = Entry(self, width=7).grid(column=3, row=2)
-        cD = Entry(self, width=7).grid(column=3, row=3)
-        cT = Entry(self, width=7).grid(column=3, row=4)
-        pos = Entry(self, width=7).grid(column=3, row=5)
-        cli = Entry(self, width=7).grid(column=3, row=6)
+        en = Entry(self, width=15).grid(column=3, row=1)
+        cS = Entry(self, width=15).grid(column=3, row=2)
+        cD = Entry(self, width=15).grid(column=3, row=3)
+        cT = Entry(self, width=15).grid(column=3, row=4)
+        pos = Entry(self, width=15).grid(column=3, row=5)
+        cli = Entry(self, width=15).grid(column=3, row=6)
 
         #botones
-        Style().configure("Button", padding=(0,5,0,5), bgcolor = "red")
-        salida = Button(self, text="Salir", width=15).grid(row=7, column=1)
-        cancel = Button(self, text="Cancelar pedido", width=15).grid(row=7, column=2)
-        submit = Button(self, text="Hacer pedido", width=15).grid(row=7, column=3)
+        salida = Button(self, text="Salir", width=15, pady=5).grid(row=7, column=1)
+        cancel = Button(self, text="Cancelar pedido", width=15, pady=5).grid(row=7, column=2)
+        submit = Button(self, text="Hacer pedido", width=15, pady=5).grid(row=7, column=3)
 
         self.pack()
 
