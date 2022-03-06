0820f6c9e566b963d22d3549372075c729778034
Pablo <78887322+monardop@users.noreply.github.com>
2022-03-05 22:16:52 -0300

    Arreglé y terminé la funcion que chequea que el vendedor cambió o no.

--- funciones_botones.py
+++ funciones_botones.py
@@ -23,16 +23,41 @@ def cartel_confirma():
         return 0
 
 def cheq_vendedor():
-    #Esta función chequea que el vendedor anterior sea el mismo que en el nuevo pedido. De serlo, se suma el total. En caso contrario, se envia los datos para llenar registro. *Sin terminar
+    #Esta función chequea que el vendedor anterior sea el mismo que en el nuevo pedido. De serlo, se suma el total. En caso contrario, se envía los datos para llenar registro. 
+    
     vendedor = str(gui.en.get())
-    with open("Vendedor.txt", "r") as f:
-        vendedor_anterior = f.read()
-    if vendedor != vendedor_anterior:
+    if vendedor == "":
+        messagebox.showerror(title="Error", message="No ingresó el nombre de vendedor")
+        return 2
+
+    try:
+        with open("Vendedor.txt", "r") as f:
+            vendedor_anterior = f.read()
+    except FileNotFoundError:
         with open("Vendedor.txt", "w") as f:
             f.write(vendedor)
+        with open("ventaTotal.txt", "w") as f:
+                f.write("0")
+        vendedor_anterior = vendedor
+    
+
+    if vendedor != vendedor_anterior:
+        try:
+            with open("ventaTotal.txt", "r") as f:
+                total = float(f.read())
+        except FileNotFoundError:
+            messagebox.showerror(title="Error", message="Ha ocurrido un error.")
+            return 2
+        llenar_registro("OUT", vendedor_anterior, total)  
+        with open("Vendedor.txt", "w") as f:
+            f.write(vendedor)
+        with open("ventaTotal.txt", "w") as f:
+            f.write("0")
         return 0
     else:
         return 1
+
+
 def cerrar_app():
     #Funcion del boton salir. Básicamente crea un .txt que guardara el nombre del ultimo vendedor 
     vendedor = str(gui.en.get())
@@ -50,12 +75,13 @@ def llenar_registro( op: str, vendedor: str, total: float):
     conn.commit()
     conn.close()
 
-def control_validez(x):
+def control_validez(x, elem: str):
     """Con esta función controlo que lo ingresado en los campos de pedidos sean nros enteros no negativos."""
     if x is isinstance(x, int):
-        if x < 0: 
+        if x <= 0: 
             return 1
     else:
+        messagebox.showerror(title="Error", message=f"Error en el ingreso de datos en {elem}")
         return 1
 
 
@@ -63,17 +89,27 @@ def crear_venta():
     """Con la variable x creo una bandera para identificar si hubo un error al cargar los datos, dado que estos serán ingresados de forma manual y estos no pueden ser algo distinto de un entero positivo.
     Una vez comprobado los datos, creo el objeto venta que se encarga de obtener precios y cargarlos al servidor. De este objeto me llevo el total de esa venta."""
     comboS = gui.cS.get()
-    x = control_validez(comboS)
+    x = control_validez(comboS, "Combo Simple")
     comboD = gui.cD.get()
-    x = control_validez(comboD)
+    x = control_validez(comboD, "Combo doble")
     comboT = gui.cT.get()
-    x = control_validez(comboT)
+    x = control_validez(comboT, "Combo triple")
     postre = gui.pos.get()
-    x = control_validez(postre)
+    x = control_validez(postre, "Postre")
     cliente = str(gui.cli.get())
+    if cliente == "":
+        messagebox.showerror(title="Error", message="Error en el ingreso de datos de cliente")
+        x = 1
     if x != 1:
         venta = ventas.Venta(cliente, comboS,comboD,comboT,postre)
         return venta.total
     else:
-        messagebox.showerror(title="Erorr", message="Error al ingresar datos")
         return 1 
+
+def suma_ventas(total: float):
+    with open("ventaTotal.txt", "w") as f:
+        antes = float(f.read())
+        total += antes
+        f.write(total)
+
+
